e2d25bb23211ec6f6b88f49f32d96f3a082712ec
Роман Лодяты Александрович <68326671+roma111ka@users.noreply.github.com>
2022-05-09 00:48:32 +0300

    feat: add animation and key events

--- src/js/Keyboard.js
+++ src/js/Keyboard.js
@@ -4,9 +4,43 @@ import { createDomNode } from './createDomNodeScript.js';
 export default class Keyboard {
     constructor(){
         this.lang = 'ru';
-        this.caps = 'off';
+        this.caps = false;
         this.shift = false;
     }
+
+    updateKeyboard(event) {
+        const { lang } = this.lang;
+        if (event.shiftKey || this.shift) {
+          document.querySelectorAll('.key__btn').forEach((e) => {
+            if (e.dataset[`${lang}Shift`]) {
+              if (this.caps === true) {
+                e.innerHTML = e.dataset[`${lang}Shift`].toLowerCase();
+              } else e.innerHTML = e.dataset[`${lang}Shift`];
+            } else if (e.dataset[lang]) e.innerHTML = e.dataset[lang];
+          });
+        } else {
+          document.querySelectorAll('.key__btn').forEach((e) => {
+            if (e.dataset[lang]) {
+              if (this.caps === true && !(event.shiftKey || this.shift)) {
+                e.innerHTML = e.dataset[lang].toUpperCase();
+              } else e.innerHTML = e.dataset[lang];
+            }
+          });
+        }
+      }
+
+    removeShift(event) {
+        if (this.shift) {
+          this.shift = !this.shift;
+          document.querySelector('.key_leftshift').classList.remove('active');
+          document.querySelector('.key_rightshift').classList.remove('active');
+          this.updateKeyboard(event);
+        }
+      }
+    changeCapsLock(event) {
+       this.caps === true ? this.caps = false: this.caps = true;
+       this.updateKeyboard(event);
+     }   
 
     generateKeyBoard(){
         const KEYBOARD = createDomNode('div', '', 'keyboard');
@@ -22,7 +56,7 @@ export default class Keyboard {
                if(element.class){
                    KEY_BTN.classList.add(element.class);
                }
-
+               KEY_BTN.dataset.code = element.code;
                KEYS_ROW.append(KEY_BTN);
            });
            KEYBOARD_CONTAINER.append(KEYS_ROW);
--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,8 @@ import { createDomNode } from './createDomNodeScript.js';
 
 const BODY = document.querySelector('body');
 const KEYBOARD = new Keyboard();
+
+
 /*creat page*/ 
 const createHeader = () =>{
     const HEADER = createDomNode('header', '', 'header');
@@ -33,6 +35,109 @@ const createFooter = () =>{
     FOOTER.append(WRAPPER);
     BODY.append(FOOTER);
 }
+/* buttons */
+
+
+const keyEvent = (event, btn, codeEvent ) =>{
+    const textField = document.querySelector('.textfield');
+    let symbol = '';
+    let cursor = textField.selectionStart;
+    event.preventDefault();
+    textField.focus();
+
+    if (codeEvent === 'CapsLock') KEYBOARD.changeCapsLock(event);
+    if (codeEvent === 'ShiftLeft' || codeEvent === 'ShiftRight') KEYBOARD.updateKeyboard(event);
+    if (btn.dataset.noType !== true) {
+        console.log("active");
+        symbol = btn.textContent;
+        KEYBOARD.removeShift(event);
+    }
+    if (codeEvent  === 'Tab') symbol = '    ';
+    if (codeEvent  === 'Enter') symbol = '\n';
+    if (codeEvent  === 'Backspace') symbol = '-1';
+    if (codeEvent  === 'Delete') symbol = '+1';
+
+    if (codeEvent === 'ArrowLeft' && cursor > 0) textField.setSelectionRange(cursor - 1, cursor - 1);
+    if (codeEvent === 'ArrowRight') {
+      cursor = textField.selectionEnd;
+      textField.setSelectionRange(cursor + 1, cursor + 1);
+    }
+  
+    if (codeEvent === 'ArrowUp') {
+      const SymbolBeforeCursor = textField.value.substring(0, cursor).split('\n');
+      if (SymbolBeforeCursor.length === 1 || SymbolBeforeCursor[SymbolBeforeCursor.length - 1].length >= 55) {
+        cursor -= 55;
+      } else if (SymbolBeforeCursor[SymbolBeforeCursor.length - 1].length <= SymbolBeforeCursor[SymbolBeforeCursor.length - 2].length % 55) {
+         cursor -= (SymbolBeforeCursor[SymbolBeforeCursor.length - 2].length % 55) + 1;
+      } else {
+        cursor -= SymbolBeforeCursor[SymbolBeforeCursor.length - 1].length + 1;
+      }
+      if (cursor < 0) cursor = 0;
+      textField.setSelectionRange(cursor, cursor);
+    }
+  
+    if (codeEvent === 'ArrowDown') {
+      cursor = textField.selectionEnd;
+      const symbolBeforeCursor = textField.value.substring(0, cursor).split('\n');
+      const symbolAfterCursor = textField.value.substring(textField.selectionEnd).split('\n');
+      if (symbolAfterCursor.length === 1 || symbolAfterCursor[0].length >= 55) {
+        cursor += 55;
+      } else if ((symbolBeforeCursor[symbolBeforeCursor.length - 1].length % 55)
+      > symbolAfterCursor[1].length) {
+        cursor += symbolAfterCursor[0].length + symbolAfterCursor[1].length + 1;
+      } else if ((((symbolBeforeCursor[symbolBeforeCursor.length - 1].length) + symbolAfterCursor[0].length) > 57)) {
+        cursor += symbolAfterCursor[0].length;
+      } else {
+        cursor += (symbolBeforeCursor[symbolBeforeCursor.length - 1].length % 57) + symbolAfterCursor[0].length + 1;
+      }
+      textField.setSelectionRange(cursor, cursor);
+    }
+
+
+    if(symbol){
+        let symbolBeforeCursor = textField.value.substring(0, cursor);
+        let symbolAfterCursor = textField.value.substring(textField.selectionEnd);
+        if (symbol === '-1') {
+            symbol = '';
+            if (cursor === textField.selectionEnd) {
+              symbolBeforeCursor = symbolBeforeCursor.slice(0, -1);
+              cursor -= (cursor > 0) ? 2 : 1;
+            } else cursor -= 1;
+        }
+        if (symbol === '+1') {
+            symbol = '';
+            if (cursor === textField.selectionEnd) {
+              symbolAfterCursor = symbolAfterCursor.slice(1);
+            }
+            cursor -= 1;
+          }
+        textField.value = symbolBeforeCursor + symbol + symbolAfterCursor;
+        textField.setSelectionRange(cursor + 1, cursor + 1);
+        if (symbol === '    ') textField.setSelectionRange(cursor + 4, cursor + 4);
+    }
+};
+
+document.addEventListener('keyup', (event) => {
+    console.log("happyUP");
+    const BUTTON = document.querySelector(`[data-code=${event.code}]`);
+    if (BUTTON) {
+        BUTTON.classList.remove('active');
+        if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
+            KEYBOARD.removeShift(event);
+        }
+    }
+});
+
+/* events */
+document.addEventListener('keydown', (event) => {
+    console.log("happyDown");
+    const BUTTON = document.querySelector(`[data-code=${event.code}]`);
+    if (BUTTON) {
+        BUTTON.classList.add('active');
+        keyEvent(event, BUTTON, event.code);
+    }
+});
+
 
 window.onload = () =>{
     createHeader();
