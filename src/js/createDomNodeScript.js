fa3de38c83a70f0fad366ea3e265032f6c6337af
Роман Лодяты Александрович <68326671+roma111ka@users.noreply.github.com>
2022-05-09 16:28:55 +0300

    fix: code style

--- src/js/createDomNodeScript.js
+++ src/js/createDomNodeScript.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line import/prefer-default-export
 export const createDomNode = (element, innerHTML, ...classes) => {
-    const node = document.createElement(element);
-    node.classList.add(...classes);
-    node.innerHTML = innerHTML;
-    return node;
+  const node = document.createElement(element);
+  node.classList.add(...classes);
+  node.innerHTML = innerHTML;
+  return node;
 };
